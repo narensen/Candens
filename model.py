ce08d4f158e2fc937e594d1b21dcc1aa58f0564f
Naren <scartegy@gmail.com>
2025-01-19 22:10:05 +0530

    model fixed

--- model.py
+++ model.py
@@ -1,16 +1,21 @@
 import torch.nn as nn
 import torchvision
+import torch
 
 class Candens(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.backbone = torchvision.models.densenet161(pretrained=True)
-        in_features = self.backbone.fc.in_features
+        in_features = self.backbone.classifier.in_features
 
         self.logit == nn.Linear(in_features, 1)
         
     def forward(self, x):
-        return self.backbone(x)
+        x = self.backbone.features(x)
+        x = torch.flatten(x)
+
+        x = self.logit(x)
+        return x
         
 
